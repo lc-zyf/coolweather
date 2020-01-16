bce710872343ebd70b97c831e21cff98d1ba324e
lc <1335616874@qq.com>
2020-01-16 18:56:00 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/Country.java
@@ -0,0 +1,49 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public Country(int id, String countryName, String weatherId, int cityId) {
+        this.id = id;
+        this.countryName = countryName;
+        this.weatherId = weatherId;
+        this.cityId = cityId;
+    }
+}
